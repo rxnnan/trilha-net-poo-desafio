e607c552823f8e47d1c17ff9eec0ae920ef2b1a5
Rennan Carbas <carbaslopes@gmail.com>
2023-12-19 16:13:06 -0300

    Desafio concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Nokia nokia = new("123456789", "Modelo XYZ", "11111111", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Candy Crush Saga");
+
+Console.WriteLine("\n--------------------------------------------------\n");
+
+Console.WriteLine("Iphone:");
+Iphone iphone = new("987654321", "14", "99999999", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("YouTube");
